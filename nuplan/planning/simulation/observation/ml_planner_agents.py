aeccbf2c306c53dc606631e65bd077f61b442493
sacardoz <sacardoz@uwaterloo.ca>
2023-12-31 02:20:57 -0500

    moving constants

--- nuplan/planning/simulation/observation/ml_planner_agents.py
+++ nuplan/planning/simulation/observation/ml_planner_agents.py
@@ -22,6 +22,9 @@ from nuplan.planning.simulation.controller.two_stage_controller import TwoStageC
 from nuplan.planning.simulation.history.simulation_history_buffer import SimulationHistoryBuffer
 from nuplan.planning.simulation.observation.abstract_observation import AbstractObservation
 from nuplan.planning.simulation.observation.mlpa.max_depth_breadth_first_search import MaxDepthBreadthFirstSearch
+from nuplan.planning.simulation.observation.mlpa.planner_config_constants import IDM_AGENT_CONFIG, OPEN_LOOP_DETECTION_TYPES, PDM_CLOSED_AGENT_CONFIG, \
+                                                                                    PDM_HYBRID_AGENT_CONFIG, PDM_BATCH_IDM_CONFIG, \
+                                                                                    PDM_OFFSET_MODEL_CONFIG
 from nuplan.planning.simulation.observation.observation_type import DetectionsTracks, Observation
 from nuplan.planning.simulation.occlusion.wedge_occlusion_manager import WedgeOcclusionManager
 from nuplan.planning.simulation.planner.abstract_planner import PlannerInitialization, PlannerInput
@@ -32,8 +35,6 @@ from nuplan.planning.simulation.simulation_time_controller.simulation_iteration 
 from nuplan.planning.simulation.trajectory.abstract_trajectory import AbstractTrajectory
 from nuplan.planning.training.modeling.torch_module_wrapper import TorchModuleWrapper
 
-from nuplan.planning.simulation.trajectory.trajectory_sampling import TrajectorySampling
-
 from tuplan_garage.planning.training.modeling.models.pdm_offset_model import PDMOffsetModel
 from tuplan_garage.planning.simulation.planner.pdm_planner.pdm_closed_planner import PDMClosedPlanner
 from tuplan_garage.planning.simulation.planner.pdm_planner.pdm_hybrid_planner import PDMHybridPlanner
@@ -41,55 +42,6 @@ from tuplan_garage.planning.simulation.planner.pdm_planner.pdm_hybrid_planner im
 from tuplan_garage.planning.simulation.planner.pdm_planner.proposal.batch_idm_policy import BatchIDMPolicy
 
 from nuplan.common.maps.maps_datatypes import SemanticMapLayer
-
-OPEN_LOOP_DETECTION_TYPES = [TrackedObjectType.PEDESTRIAN, TrackedObjectType.BICYCLE, \
-                             TrackedObjectType.CZONE_SIGN, TrackedObjectType.BARRIER, \
-                             TrackedObjectType.TRAFFIC_CONE, TrackedObjectType.GENERIC_OBJECT]
-
-
-# Taken from idm_planner.yaml
-IDM_AGENT_CONFIG = {  
-    "target_velocity": 10,             # Desired velocity in free traffic [m/s]
-    "min_gap_to_lead_agent": 1.0,      # Minimum relative distance to lead vehicle [m]
-    "headway_time": 1.5,               # Desired time headway. The minimum possible time to the vehicle in front [s]
-    "accel_max": 1.0,                  # Maximum acceleration [m/s^2]
-    "decel_max": 3.0,                  # Maximum deceleration (positive value) [m/s^2]
-    "planned_trajectory_samples": 16,  # Number of trajectory samples to generate
-    "planned_trajectory_sample_interval": 0.5,  # The sampling time interval between samples [s]
-    "occupancy_map_radius": 40,        # The range around the ego to add objects to be considered [m]
-}
-
-# Taken from pdm_closed_planner.yaml in the TuPlan code
-PDM_CLOSED_AGENT_CONFIG = {  
-    "trajectory_sampling": TrajectorySampling(num_poses=80, interval_length= 0.1),
-    "proposal_sampling": TrajectorySampling(num_poses=40, interval_length= 0.1),
-    "lateral_offsets": [-1.0, 1.0], 
-    "map_radius": 50,
-}
-
-PDM_BATCH_IDM_CONFIG = {
-    "speed_limit_fraction":[0.2,0.4,0.6,0.8,1.0], 
-    "fallback_target_velocity":15.0, 
-    "min_gap_to_lead_agent":1.0,
-    "headway_time":1.5,
-    "accel_max":1.5,
-    "decel_max":3.0
-}
-
-# Taken from pdm_hybrid_planner.yaml in the TuPlan code
-PDM_OFFSET_MODEL_CONFIG = {
-    "trajectory_sampling": TrajectorySampling(num_poses=16, interval_length=0.5), 
-    "history_sampling": TrajectorySampling(num_poses=10, interval_length=0.2),
-    "planner": None,
-    "centerline_samples": 120,
-    "centerline_interval": 1.0,
-    "hidden_dim": 512
-}
-
-PDM_HYBRID_AGENT_CONFIG = {  
-    "correction_horizon": 2.0,
-}
-
 
 class MLPlannerAgents(AbstractObservation):
     """
